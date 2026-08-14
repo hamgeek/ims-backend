d2feb96d6feaeeecb502293549cf448937ea78d4
hamgeek <ham.intl.id@gmail.com>
2026-08-15 01:32:43 +0700

    enable secureCookies

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -9,4 +9,7 @@ export const auth = betterAuth({
   baseURL: env.BETTER_AUTH_URL,
   emailAndPassword: { enabled: true },
   trustedOrigins: [env.FRONTEND_URL],
+  advanced: {
+    useSecureCookies: true,
+  },
 });
